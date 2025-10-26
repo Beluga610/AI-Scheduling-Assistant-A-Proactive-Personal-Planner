0d64dc158ed267d81669ee7f36cc4e97109421e3
yizhuo2002 <108623355+yizhuo2002@users.noreply.github.com>
2025-10-26 21:33:29 +0800

    FEATURE: Initial version which connects backend frontend and agents to play a simple game.

---
+++ frontend-react/src/main.tsx
@@ -0,0 +1,14 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
