8a9bc609232c213d3e51d071beef150b574063d6
Xiao-LJ <rimbaudxlj@gmail.com>
2025-11-02 02:26:43 +0800

    fix: update document title to 'Smart Calendar'

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './graphql/client';
 import App from './App';
+import './App.css';
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <RouterProvider router={router}>
     </ApolloProvider>
   </React.StrictMode>
 );
